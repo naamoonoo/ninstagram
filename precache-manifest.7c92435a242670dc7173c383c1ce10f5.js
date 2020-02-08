bfe658736e24dc3e70f8db8b66820e0664f66b5a
naamoonoo <naamoonoo.y@gmail.com>
2020-02-08 23:33:53 +0100

    Updates

---
+++ precache-manifest.7c92435a242670dc7173c383c1ce10f5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243d16e6663707a7d5d2eb481f965fc6",
+    "url": "/ninstagram/index.html"
+  },
+  {
+    "revision": "60259847cea4c1e7fb9d",
+    "url": "/ninstagram/static/css/2.c829125e.chunk.css"
+  },
+  {
+    "revision": "60259847cea4c1e7fb9d",
+    "url": "/ninstagram/static/js/2.b8d1c5e2.chunk.js"
+  },
+  {
+    "revision": "02d01b813c84dbaab54999c3d737ad49",
+    "url": "/ninstagram/static/js/2.b8d1c5e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e064bb1f811b41c72c",
+    "url": "/ninstagram/static/js/main.9652b7bd.chunk.js"
+  },
+  {
+    "revision": "c834b191b7706a9b446d",
+    "url": "/ninstagram/static/js/runtime-main.aa88f905.js"
+  },
+  {
+    "revision": "4bd05c92f0f1b8120ce9787c53252744",
+    "url": "/ninstagram/static/media/TopButton.4bd05c92.svg"
+  },
+  {
+    "revision": "0c50775d6bbe04f4375afea9a1f95eb2",
+    "url": "/ninstagram/static/media/backArrow.0c50775d.svg"
+  },
+  {
+    "revision": "b60cb1d71acadd4038d2ad3f7b7a3482",
+    "url": "/ninstagram/static/media/camera.b60cb1d7.svg"
+  },
+  {
+    "revision": "f81d7b25ca9a5f92ad7ce1873b870d3a",
+    "url": "/ninstagram/static/media/comment-empty.f81d7b25.svg"
+  },
+  {
+    "revision": "a91cd8320419954881c565a3f1de5390",
+    "url": "/ninstagram/static/media/comment-full.a91cd832.svg"
+  },
+  {
+    "revision": "540a3ddee6652a69e9da97ea67ceb122",
+    "url": "/ninstagram/static/media/delete.540a3dde.svg"
+  },
+  {
+    "revision": "b15629e047c5506bcf840495314bd58f",
+    "url": "/ninstagram/static/media/like-empty.b15629e0.svg"
+  },
+  {
+    "revision": "88bf2af298cb2a7a61550518719e6d96",
+    "url": "/ninstagram/static/media/like-full.88bf2af2.svg"
+  },
+  {
+    "revision": "a9b2f936377a23cff6892814d1f56a83",
+    "url": "/ninstagram/static/media/login.a9b2f936.svg"
+  },
+  {
+    "revision": "f4059304f3b2c6cb1408a97a8b6f5ec7",
+    "url": "/ninstagram/static/media/menu-dot.f4059304.svg"
+  },
+  {
+    "revision": "e94bed210ac3961808d13518798f7344",
+    "url": "/ninstagram/static/media/photoUpload.e94bed21.svg"
+  },
+  {
+    "revision": "e81447c94e918bb95e76622b68d47f2b",
+    "url": "/ninstagram/static/media/user.e81447c9.svg"
+  }
+]);
